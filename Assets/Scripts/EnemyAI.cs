a04408f6dfaa674e8a80693cf361c460cdfd2bc3
rivai izari <rivaiizari@gmail.com>
2018-10-29 06:19:39 +0700

    last

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Awake() {
+		myTransform = transform;
+	}
+
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+		target = go.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {    
+		Vector3 dir = target.position - myTransform.position;
+		dir.z = 0.0f; // Only needed if objects don't share 'z' value
+		if (dir != Vector3.zero) {
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, 
+				Quaternion.FromToRotation(Vector3.right, dir), rotationSpeed * Time.deltaTime);
+		}
+
+		//Move Towards Target
+		myTransform.position += (target.position - myTransform.position).normalized * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Tomato.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Tomato : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+	private Rigidbody2D myRigidbody;
+	private Vector2 direction;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+
+	void FixedUpdate()
+	{
+		myRigidbody.velocity = direction * speed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Initialize(Vector2 direction)
+	{
+		this.direction = direction;
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+}
